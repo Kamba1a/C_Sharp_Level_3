8b547391c78a1823f39495f19e87e9773fc8e442
Kamba1a <headcr@bk.ru>
2019-07-29 23:48:52 +0300

    Добавлены кнопки: - временно удалить получателя из списка (не из БД) - восстановить список получателей из БД

--- Lesson3/MailSender/ViewModel/EmailInfoModel.cs
+++ Lesson3/MailSender/ViewModel/EmailInfoModel.cs
@@ -2,20 +2,24 @@
 using MailSender.Services;
 using MailSender.DataClasses;
 using System.Windows;
+using System.ComponentModel;
 
 namespace MailSender.ViewModel
 {
     /// <summary>
     /// ViewModel для EmailInfoView (UserControl для работы со списком получателей)
     /// </summary>
-    public class EmailInfoModel
+    public class EmailInfoModel: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         DataAccessService _dataAccessService;                                   //класс для работы с БД
         public static ObservableCollection<Emails> EmailsCol { get; set; }      //коллекция емейлов получателей (всех)
         public static ObservableCollection<Emails> EmailsSearch { get; set; }   //коллекция емейлов получателей (только искомых)
-        public string Name { get; set; }                                        //принимает имя получателя для поиска в списке получателей
         public RelayCommand SearchCommand { get; set; }                         //команда для поиска в списке получателей
-        public RelayCommand DeleteEmailCommand { get; set; }                    //команда для удаления получателя из списка (из базы и коллекций)
+        public RelayCommand DeleteEmailCommand { get; set; }                    //команда для удаления получателя из списка (из базы и коллекции)
+        public RelayCommand TempDeleteEmailCommand { get; set; }                //команда для удаления получателя из списка (только из коллекции, не из БД)
+        public RelayCommand GetEmailsCommand { get; set; }                      //команда получения списка получателей из БД
+        string _name;                                                           //принимает имя получателя для поиска в списке получателей
 
         public EmailInfoModel()
         {
@@ -24,20 +28,37 @@ namespace MailSender.ViewModel
             EmailsSearch = new ObservableCollection<Emails>();
             SearchCommand = new RelayCommand(SearchEmail);
             DeleteEmailCommand = new RelayCommand(DeleteEmail);
+            TempDeleteEmailCommand = new RelayCommand(TempDeleteEmail);
+            GetEmailsCommand = new RelayCommand(GetEmails);
             GetEmails();
-            CopyEmails();
+        }
+
+        /// <summary>
+        /// принимает имя получателя для поиска в списке получателей
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
         }
 
         /// <summary>
         /// Получение из БД списка всех получателей
         /// </summary>
-        void GetEmails()
+        void GetEmails(object obj=null)
         {
             EmailsCol.Clear();
             foreach (var item in _dataAccessService.GetEmails())
             {
                 EmailsCol.Add(item);
             }
+            EmailsSearch.Clear();
+            CopyEmails();
+            Name = null;
         }
 
         /// <summary>
@@ -62,6 +83,7 @@ namespace MailSender.ViewModel
             {
                 foreach (Emails s in EmailsCol)
                 {
+                    if (string.IsNullOrEmpty(s.Name)) continue;
                     if (s.Name.ToLower().Contains(Name.ToLower()))
                     {
                         EmailsSearch.Add(s);
@@ -82,6 +104,27 @@ namespace MailSender.ViewModel
                 GetEmails();
                 SearchEmail();
             }
+        }
+
+        /// <summary>
+        /// Удаление получателя только из коллекции (не из БД)
+        /// </summary>
+        void TempDeleteEmail(object obj)
+        {
+            if (obj == null) MessageBox.Show("Выберите получателя");
+            else
+            {
+                EmailsCol.Remove(obj as Emails);
+                SearchEmail();
+            }
+        }
+
+        /// <summary>
+        /// реализация интерфейса INotifyPropertyChanged
+        /// </summary>
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
