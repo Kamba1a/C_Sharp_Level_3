daad6420f07640159b1104b8422321e83c2ab568
Kamba1a <headcr@bk.ru>
2019-08-16 19:28:21 +0300

    Урок 5. Задание 1.

---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//2.	* Написать приложение, выполняющее парсинг CSV-файла произвольной структуры и сохраняющего его в обычный txt-файл.
+//    Все операции проходят в потоках.CSV-файл заведомо имеет большой объем.
+
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
