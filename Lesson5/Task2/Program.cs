18af6382012f47e0f2a75062daa51fefbc0188a3
Kamba1a <headcr@bk.ru>
2019-08-28 12:33:46 +0300

    Урок 5. Переделано задание №2 Теперь программа разбивает файл на несколько частей, каждая из которых записывается в отдельном потоке - т.к. условие задачи не содержит никакой конкретики, такое решение показалось наиболее логичным для использования потоков

--- Lesson5/Task2/Program.cs
+++ Lesson5/Task2/Program.cs
@@ -11,76 +11,66 @@ using System.Threading.Tasks;
 //    Все операции проходят в потоках.CSV-файл заведомо имеет большой объем.
 
 
-    //Не очень понятно, что именно хотят в задании, т.к. насколько я понимаю считывание и запись файла в принципе происходит только в одном в потоке
-    //В первом варианте попыталась сделать считывание и запись по принципу 1 строка-1 поток, но по-моему все это как-то бессмысленно
-    //Во втором просто вынесла операцию чтения/записи в отдельный поток, но наверное это не то, что было нужно, тк для задания со звездочкой это как-то слишком просто
-
 namespace Task2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            #region Вариант 1 - попытка сделать считывание и запись файла по строкам - не знаю, как закрыть стримы ПОСЛЕ того, как отработали ВСЕ вторичные потоки
-            /*
-            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
-            object forLocker = new object();
-            StreamReader sr = new StreamReader("myTestCSV.csv");
-            StreamWriter sw = new StreamWriter("myTestTXT.txt");
+            //Программа считывает файл и разбивает его на несколько частей указанного размера, после чего сохраняет их в формате .txt
+            //Сохранение каждой одтельной части файла реализовано в отдельных потоках
 
-            //ThreadPool.SetMinThreads(2, 2);
-            //ThreadPool.SetMaxThreads(Environment.ProcessorCount, Environment.ProcessorCount);
+            Console.WriteLine("Программа разбивает файл на несколько частей в зависимости от указанного размера, и сохраняет их в формате .txt");
 
-            string line;
-            while (!sr.EndOfStream)
+            int filePartSize;   //указанный размер одной части файла в байтах
+            do { Console.WriteLine("Укажите желаемый размер части (в байтах), например, 256"); }
+            while (!int.TryParse(Console.ReadLine(), out filePartSize));
+
+            using (FileStream fileReader = File.OpenRead("myTestCSV.csv"))
             {
-                //ThreadPool.QueueUserWorkItem(o => myMethod());    //с ThreadPool почему-то вообще не работает толком
-                Thread thread = new Thread(() => myMethod());
-                thread.Start();
-                //thread.Join();    //если сделать так, то главный поток будет дожидаться завершения каждого из вторичных потоков,но смысл потоков по-моему вообще исчезает
-            }
-
-            autoResetEvent.WaitOne();   //так главный поток останавливается, но непонятно, как уведомить его, что ВСЕ вторичные потоки отработали, чтобы можно было закрыть StreamReader и StreamWriter
-
-            sr.Close();
-            sw.Close();
-            Console.WriteLine("Потоки StreamReader и StreamWriter закрыты");
-
-            void myMethod()
-            {
-                lock (forLocker)
+                long fileSize = fileReader.Length;  //размер считываемого файла в байтах
+                if (fileSize <= filePartSize)   //если размер файла меньше указанного размера части, то просто записываем один txt файл
                 {
-                    line = sr.ReadLine();
-                    Console.WriteLine(line);
-                    if (line != null)
+                    int fileSizeInt = (int)fileSize;
+                    byte[] textByte = new byte[fileSize];
+                    fileReader.Read(textByte, 0, fileSizeInt);
+                    using (FileStream fileWriter = new FileStream("myTestTXT.txt", FileMode.Create))
                     {
-                        sw.WriteLine(line, true);
-                        Console.WriteLine("Строка записана в txt файл");
+                        fileWriter.Write(textByte, 0, fileSizeInt);
                     }
-                    //autoResetEvent.Set();     //если сделать так, то флаг снимается уже после первого отработавшего потока, поэтому это не подходит для уведомления главного потока о завершении ВСЕХ потоков
+                }
+                else //иначе записываем несколько частей
+                {
+                    double filePartsAmount = Math.Ceiling((double)fileSize / filePartSize);     //определяет количество записываемых частей файла
+                    for (int i = 1; i <= filePartsAmount; i++)
+                    {
+                        byte[] textByte = new byte[filePartSize];
+                        Thread.Sleep(2000); //имитация чтения части файла большого объема
+                        fileReader.Read(textByte, 0, filePartSize);
+
+                        int filePartNumber = i; //переменная для передачи потокам в качестве параметра
+                        ThreadPool.QueueUserWorkItem((obj) => CreatePartTxtFile(textByte, filePartNumber));
+
+                        //или тоже самое через ручное создание потоков:
+                        //Thread thread = new Thread(()=> CreatePartTxtFile(textByte,filePartNumber));
+                        //thread.Start();
+                    }
                 }
             }
-            Console.ReadKey();
-            */
-            #endregion
-
-            #region Вариант 2 - операция чтения/записи просто запущена в отдельном потоке
-
-            StreamReader sr = new StreamReader("myTestCSV.csv");
-            StreamWriter sw = new StreamWriter("myTestTXT.txt");
-
-            Thread thread = new Thread(() => sw.Write(sr.ReadToEnd()));
-            thread.Start();
-
-            thread.Join();
-
-            sr.Close();
-            sw.Close();
-            Console.WriteLine("Данные из csv файла записаны в txt");
-
+            Console.WriteLine("Конец работы главного потока");
             Console.ReadKey();
 
-            #endregion
+            //запись одной части файла
+            void CreatePartTxtFile(byte[] textByteArray, int filePartNumber)
+            {
+                using (FileStream fileWriter = new FileStream("myTestTXT_p" + filePartNumber + ".txt", FileMode.Create))
+                {
+                    Console.WriteLine("Начало записи части " + filePartNumber);
+                    Thread.Sleep(5000); //имитация записи большой части файла
+                    fileWriter.Write(textByteArray, 0, filePartSize);
+                    Console.WriteLine("Часть "+ filePartNumber + " записана");
+                }
+            }
         }
     }
 }
